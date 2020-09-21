a30f4967fbccb8a3399c0bda869bc76449c8e2f3
Arnaud BODEL <contact@osw3.net>
2020-09-21 12:08:16 +0200

    -

--- TP-01/index.php
+++ TP-01/index.php
@@ -2,3 +2,16 @@
 // Ajouter votre réponse sur le formulaire :
 // https://forms.gle/oFeunAjpJrskBrHeA
 
+
+// Récupèrer les données du tableau pour effectuer un addition dont le résultat sera 142
+$num = [42, 100];
+
+
+// Afficher la phrase "Bob cultive du maïs et du blé". 
+$assoc = [
+    'name' => "Bob",
+    'farm' => [
+        "maïs",
+        "blé"
+    ]
+];
