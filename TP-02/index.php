c354a1df937f2e0ff04400bc521a45338beb2362
Arnaud BODEL <contact@osw3.net>
2020-09-21 12:21:32 +0200

    -

---
+++ TP-02/index.php
@@ -0,0 +1,20 @@
+<?php 
+// Ajouter votre réponse sur le formulaire :
+// https://forms.gle/ysYGhm4YKgihMVz48
+
+$arr = [
+    [
+        'name' => "Batman",
+        'powers' => ["Belt", "Money"]
+    ],
+    [
+        'name' => "Superman",
+        'powers' => ["Lazer eyes", "Fly", "Speed"]
+    ]
+];
+
+// Afficher la liste des heros dans une liste de définition HTML
+// Batman
+//      Belt, Money
+// Superman
+//      Lazer eyes, Fly, Speed
