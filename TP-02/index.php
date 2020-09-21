c9cea18e4dd7a1476d88d9a77dd9de7764810684
Arnaud BODEL <contact@osw3.net>
2020-09-21 16:01:49 +0200

    -

--- TP-02/index.php
+++ TP-02/index.php
@@ -2,6 +2,11 @@
 // Ajouter votre réponse sur le formulaire :
 // https://forms.gle/ysYGhm4YKgihMVz48
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $arr = [
     [
         'name' => "Batman",
@@ -23,7 +28,6 @@ $arr = [
 // instruction php "for" ou "foreach"
 // instruction php "implode"
 
-
 ?>
 
 <h2>Rendu souhaité</h2>
@@ -38,3 +42,34 @@ $arr = [
 </dl>
 
 <hr>
+
+
+<h2>Corretion 1</h2>
+
+<?php
+
+echo "<dl>";
+
+// foreach ($array as $value)
+foreach ($arr as $hero)
+{
+    echo "<dt>" . $hero['name'] . "</dt>";
+    echo "<dd>" . implode(", ", $hero['powers']) . "</dd>";
+}
+
+echo "</dl>";
+
+?>
+
+<hr>
+
+
+<h2>Corretion 2</h2>
+
+<dl>
+<?php foreach( $arr as $hero ): ?>
+    <dt><?= $hero['name'] ?></dt>
+    <dd><?= implode(", ", $hero['powers']) ?></dd>
+<?php endforeach; ?>
+</dl>
+
