60e5bebfab70256a559980c715785b7068d7cf2e
Arnaud BODEL <contact@osw3.net>
2020-09-22 14:18:48 +0200

    -

---
+++ TP-04/index.php
@@ -0,0 +1,22 @@
+<?php
+// Ajouter votre réponse sur le formulaire :
+// https://forms.gle/TxhzmpEx3oW67aBf7
+
+// Création de deux tables
+// - users 
+// - articles 
+
+// Champs de la table Users
+// --
+// id
+// firstname
+// lastname
+// email
+// password
+
+// Champs de la table Articles
+// --
+// id
+// title
+// content
+// read_counter
---
+++ bdd/cours.sql
@@ -0,0 +1,16 @@
+-- Nom du Schema
+
+-- Nom des tables
+
+
+-- Les champs / Colonnes
+-- Le nom de la colonne
+-- Type de donnée (INT, CHAR, VARCHAR)
+-- PK : Primary Key, clé primaire
+-- NN : Not Null
+-- UQ : Uniq
+-- BIN : Binary
+-- UN : UNsigned
+-- ZF : ZeroFill
+-- AI : Auto Increment
+-- G: Generated
